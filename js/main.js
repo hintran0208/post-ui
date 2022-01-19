060ba7d1d6c668f0a2f02d05e4b191703cdb7586
Hien Tran <tranthanhhien5@gmail.com>
2022-01-19 14:55:39 +0700

    add postApi studentApi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log('Hello')
+import postApi from './api/postApi'
+
+async function main() {
+  const queryParams = {
+    _page: 1,
+    _limit: 5,
+  }
+
+  const response = await postApi.getAll(queryParams)
+  console.log(response)
+}
+
+main()
